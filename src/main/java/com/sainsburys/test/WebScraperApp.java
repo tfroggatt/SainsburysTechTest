79a5963d088ac9f7ef308114af2de808adc842b3
Froggatt <tfroggatt@ptc.com>
2018-11-22 09:21:05 +0000

    Altering entry point to better handle inputed URL
    Modifying the entry point class for the application and created 2
    constructors that are called from the main method. Which one is called
    based upon whther the user has entered a URL as a command line argument
    or not (which will use the default URL).

--- src/main/java/com/sainsburys/test/WebScraperApp.java
+++ src/main/java/com/sainsburys/test/WebScraperApp.java
@@ -10,44 +10,56 @@ import com.sainsburys.test.scraper.Scraper;
 import com.sainsburys.test.transform.TransformToJson;
 
 /**
- * This is the entry point for the Web Scraper application. It can take in urls via 
- * command line arguments, or use a dummy url if non are given.
+ * This is the entry point for the Web Scraper application. It can take in urls via command line arguments, or use a
+ * dummy url if non are given.
  */
 public class WebScraperApp {
-	
+
+    private String webPageUrl = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
+
     public static void main(String[] args) throws IOException {
-    	if(args.length > 0) {
-    		for (String url : args) {
-    			scrapeUrl(url);
-    		}
-    	} else {
-    		scrapeUrl(getUrlString());
-    	}
-    	
+        if (args.length > 0) {
+            WebScraperApp givenUrlApp = new WebScraperApp(args[0]);
+            givenUrlApp.scrapeUrl();
+        } else {
+            WebScraperApp defaultUrlApp = new WebScraperApp();
+            defaultUrlApp.scrapeUrl();
+        }
+
     }
-    
+
     /**
-     * Will take the URL provide, create a scraper and pull out the relevant information before calling
-     * a transformer to transform the found content to JSON which is printed to the console.
+     * Default constructor to create an instance of the application with a default URL.
+     */
+    public WebScraperApp() {
+    }
+
+    /**
+     * Constructor to create an instance of the application with the URL for the webpage to scrap given as the first
+     * command line argument.
      * 
-     * @param url The URL of the web page to scrape
+     * @param webPageUrl
+     *            The new URL to scrape
+     */
+    public WebScraperApp(String webPageUrl) {
+        this.webPageUrl = webPageUrl;
+    }
+
+    /**
+     * Will take the URL provide, create a scraper and pull out the relevant information before calling a transformer to
+     * transform the found content to JSON which is printed to the console.
+     * 
+     * @param url
+     *            The URL of the web page to scrape
      * @throws IOException
      */
-    public static void scrapeUrl(String url) throws IOException {
-        Scraper scraper = new Scraper(url);
+    public void scrapeUrl() throws IOException {
+        Scraper scraper = new Scraper(webPageUrl);
         List<Product> products = scraper.scrapeWebPage();
-        
+
         TransformToJson transformer = new TransformToJson();
         JSONObject json = transformer.createJsonForProducts(products);
         System.out.println(json);
     }
-    
-    /**
-     * This returns the URL to use if non were provided through the command line.
-     * 
-     * @return The URL as a string
-     */
-    public static String getUrlString() {
-    	return "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
-    }
+
 }
--- src/test/java/com/sainsburys/test/WebScraperAppTest.java
+++ src/test/java/com/sainsburys/test/WebScraperAppTest.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Test;
 
 import com.sainsburys.test.products.Product;
@@ -15,74 +13,73 @@ import mockit.Expectations;
 import mockit.Mocked;
 
 public class WebScraperAppTest {
-	
-	/**
-	 * Tests that when no command line argument urls are given, the scrape method is
-	 * called on the default url.
-	 */
-	@Test
-	public void testMain_noArgs_defaultUrlUsed() throws IOException {
-		
-		new Expectations(WebScraperApp.class) {
-			{
-				//Acts as a verification
-				WebScraperApp.scrapeUrl(WebScraperApp.getUrlString());
-			}
-		};
-		
-		WebScraperApp.main(new String[0]);
-	}
-	
-	/**
-	 * Tests that when command line argument url are given, the scrape method is called 
-	 * on the given url.
-	 */
-	@Test
-	public void testMain_urlArgsGiven_givenUrlUsed() throws IOException {
-		
-		String[] urls = new String[1];
-		urls[0] = "http://www.google.com";
-		
-		new Expectations(WebScraperApp.class) {
-			{
-				//Acts as a verification
-				WebScraperApp.scrapeUrl("http://www.google.com");
-			}
-		};
-		
-		WebScraperApp.main(urls);
-	}
-	
-	/**
-	 * Tests that the code to scrape the URL given and then the transformation to turn the results to JSON
-	 * are called when run.
-	 */
-	@Test
-	public void testScrapeUrl_noErrorsThrown(@Mocked Scraper scraper) throws IOException {
-		
-		List<Product> products = new ArrayList<>();
-		products.add(new Product("name", "description", "10.00"));
-		
-		new Expectations() {
-			{
-				new Scraper("http://www.google.com");
-				result = scraper;
-				
-				scraper.scrapeWebPage();
-				result = products;
-			}
-		};
-		
-		WebScraperApp.scrapeUrl("http://www.google.com");
-		
-	}
-	
-	/**
-	 * Tests that the URL string returned to be scraped is the default one.
-	 */
-	@Test
-	public void testGetUrlString_defaultString() {
-		Assert.assertThat("The URL returned was not expected", WebScraperApp.getUrlString(), 
-			CoreMatchers.is("https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html"));
-	}
+
+    /**
+     * Tests that when no command line argument urls are given, the scrape method is called on the default url.
+     */
+    @Test
+    public void testMain_noArgs_defaultUrlUsed(@Mocked Scraper scraper) throws IOException {
+
+        new Expectations() {
+            {
+                new Scraper(
+                        "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html");
+                result = scraper;
+
+                scraper.scrapeWebPage();
+                result = new ArrayList<>();
+            }
+        };
+
+        WebScraperApp.main(new String[0]);
+    }
+
+    /**
+     * Tests that when command line argument url are given, the scrape method is called on the given url.
+     */
+    @Test
+    public void testMain_urlArgsGiven_givenUrlUsed(@Mocked Scraper scraper) throws IOException {
+
+        String[] urls = new String[1];
+        urls[0] = "http://www.google.com";
+
+        new Expectations() {
+            {
+                new Scraper("http://www.google.com");
+                result = scraper;
+
+                scraper.scrapeWebPage();
+                result = new ArrayList<>();
+            }
+        };
+
+        WebScraperApp.main(urls);
+    }
+
+    /**
+     * Tests that the code to scrape the URL given and then the transformation to turn the results to JSON are called
+     * when run.
+     */
+    @Test
+    public void testScrapeUrl_noErrorsThrown(@Mocked Scraper scraper) throws IOException {
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("name", "description", "10.00"));
+
+        WebScraperApp giveUrlApp = new WebScraperApp("http://www.google.com");
+
+        new Expectations() {
+            {
+                new Scraper("http://www.google.com");
+                result = scraper;
+
+                scraper.scrapeWebPage();
+                result = products;
+            }
+        };
+
+        giveUrlApp.scrapeUrl();
+
+    }
+
 }
